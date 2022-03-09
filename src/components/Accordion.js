5379c38c0d0cdf4f63ff53b3112f8b75949a5c13
Laura Travin <laura.travin@gmail.com>
2022-03-09 18:08:22 -0500

    layout2

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,12 +3,12 @@ import React from "react";
 const Accordion = ({ items }) => {
     const renderedItems = items.map ( i => {
 
-        return <div key="i.title">
+        return <React.Fragment key="i.title">
               <div className="title active">
                     <i className="dropdown icon"></i> {i.title}
               </div>
               <div className="content active"><p> {i.content} </p>  </div>
-        </div>
+        </React.Fragment>
     })
    return <div className="ui styled accordion">{renderedItems}</div>
 }
