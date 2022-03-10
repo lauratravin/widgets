669aa246ae3d7821429fd67559fb3a9b00c22908
Laura Travin <laura.travin@gmail.com>
2022-03-10 15:59:26 -0500

    active class

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,24 +1,34 @@
 import React from "react";
+import {useState} from "react";
 
 
 const Accordion = ({ items }) => {
 
+    const[activeIndex, setActiveIndex] = useState(null)
+
+
     const onTitleclick = (index) => {
-        console.log(index)
+        setActiveIndex(index)
     }
 
+
+
     const renderedItems = items.map ( (i,index) => {
+     //if the index click by user is equal to the one I am going to render, then assign the active class
+        const active = index === activeIndex? 'active' : ''
 
         return (
         <React.Fragment key={i.title}>
-              <div className="title active"
+              <div className={"title" + active}
               onClick={() => onTitleclick(index)}>
                     <i className="dropdown icon"></i> {i.title}
               </div>
-              <div className="content active"><p> {i.content} </p>  </div>
+              <div className={`content +${active}}><p> {i.content} </p>  </div>
         </React.Fragment>)
     })
-   return <div className="ui styled accordion">{renderedItems}</div>
+   return <div className="ui styled accordion">{renderedItems}
+          <h1>{activeIndex}</h1>
+          </div>
 }
 
 export default Accordion;
