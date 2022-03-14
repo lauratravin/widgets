197b0c3a47d2407ec479af9c3c7b4e8085faa316
Laura Travin <laura.travin@gmail.com>
2022-03-14 19:41:08 -0400

    axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
 
 const[term, setTerm] = useState('')
+
+useEffect(() => {
+   console.log("dfsdf")
+},[term])
 
     return (
         <div>
